4b4aceb1ca8f9f32f66d705cb8d78c84db9d720e
mrodenhauser <mrodenhauser@gmail.com>
2022-06-22 18:13:12 -0500

    updated backup connection

--- database.js
+++ database.js
@@ -4,10 +4,7 @@ const monitor = require('pg-monitor');
 
 // Loading and initializing the library:
 const initOptions = {
-    promiseLib: Promise,
-    ///ssl:{
-    //    rejectUnauthorized: false
-    //}
+    promiseLib: Promise
 };
 
 // Loading and initializing the library:
@@ -21,7 +18,7 @@ monitor.attach(initOptions)
 // const cn = 'postgresql://postgres:Jun3 Bug@localhost:5432/postgres';
 
 // env
-let cn = process.env.DATABASE_URL || 'postgres://ncgipwbjvdimxn:c84b682ba9e35e7fb1a5319321d3369a3d655c6c8972c758aa5627df7681b272@ec2-3-224-8-189.compute-1.amazonaws.com:5432/dch0qkdk3h6iia';
+let cn = process.env.DATABASE_URL || 'postgres://khjwxjvjkxxldh:24935094ee474f4fc65f2dfeb6d4e4eee67036bafc9473751f67eb411ce20c01@ec2-107-22-245-82.compute-1.amazonaws.com:5432/d3jq4rrtb9856q';
 
 
 // Creating and exporting a new database instance from the connection details:
