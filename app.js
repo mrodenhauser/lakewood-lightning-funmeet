58c37d65b3a9c36d82549e7c768302d46f43a1a4
mrodenhauser <mrodenhauser@gmail.com>
2022-06-22 19:43:00 -0500

    removed 'views/' to view paths, apparently not the problem.

--- app.js
+++ app.js
@@ -43,9 +43,10 @@ app.use('/',indexRouter);
 //static pages
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-app.get('/register', (req, res) => res.render('views/pages/register'));
-app.get('/login', (req, res) => res.render('views/pages/login'));
-app.get('/admin-home', (req, res) => res.render('views/pages/AdminHome'));
+app.get('/register', (req, res) => res.render('pages/register'));
+app.get('/login', (req, res) => res.render('pages/login'));
+app.get('/', (req, res) => res.render('pages/login'));
+app.get('/admin-home', (req, res) => res.render('pages/AdminHome'));
 
 
 // catch 404 and forward to error handler
