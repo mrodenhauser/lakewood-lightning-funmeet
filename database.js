ef769e703e0093cc07883efbea3a0a997ba324cf
mrodenhauser <mrodenhauser@gmail.com>
2022-06-26 21:08:45 -0500

    Attempted fix to SSL DB connection issue

--- database.js
+++ database.js
@@ -3,22 +3,29 @@
 const monitor = require('pg-monitor');
 
 // Loading and initializing the library:
-const initOptions = {
-    promiseLib: Promise
+//const initOptions = {
+//    promiseLib: Promise
+//};
+
+const config = {
+    promiseLib: Promise,
+    connectionString: process.env.DATABASE_URL || 'postgresql://postgres:Jun3 Bug@localhost:5432/postgres',
+    max: process.env.MAX_DB_CONNS || 20,
+    ssl:ssl
 };
 
 // Loading and initializing the library:
-const pgp = require('pg-promise')(initOptions);
+const pgp = require('pg-promise')();
 
-monitor.attach(initOptions)
+monitor.attach(config);
 
 // Preparing the connection details:
 
 
 //  process.env or local test db
-let cn = process.env.DATABASE_URL || 'postgresql://postgres:Jun3 Bug@localhost:5432/postgres';
+//let cn = process.env.DATABASE_URL || 'postgresql://postgres:Jun3 Bug@localhost:5432/postgres';
 
 
 // Creating and exporting a new database instance from the connection details:
-exports.db = pgp(cn);
+exports.db = pgp(config);
 exports.promsise = pgp;
